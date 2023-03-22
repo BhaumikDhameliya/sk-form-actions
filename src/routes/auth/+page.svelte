d0750fe997a9496715ff773b0d8a3b8d53d58df2
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-22 23:49:34 +0530

    Default Form Actions

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,24 +1,25 @@
 <script>
-	let username = '';
-	let password = '';
-	let message = '';
-	const handleSubmit = async () => {
-		message = '';
-		const response = await fetch('/auth', {
-			method: 'POST',
-			body: JSON.stringify({
-				username,
-				password
-			})
-		});
-		const data = await response.json();
-		message = data.message;
-	};
+	export let form;
+	// let username = '';
+	// let password = '';
+	// let message = '';
+	// const handleSubmit = async () => {
+	// 	message = '';
+	// 	const response = await fetch('/auth', {
+	// 		method: 'POST',
+	// 		body: JSON.stringify({
+	// 			username,
+	// 			password
+	// 		})
+	// 	});
+	// 	const data = await response.json();
+	// 	message = data.message;
+	// };
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-	<p>{message}</p>
-	<input type="text" name="username" placeholder="username" bind:value={username} />
-	<input type="password" name="password" placeholder="Password" bind:value={password} />
+<form on:submit|preventDefault={handleSubmit} method="post">
+	<p>{form?.message || ''}</p>
+	<input type="text" name="username" placeholder="username" />
+	<input type="password" name="password" placeholder="Password" />
 	<button type="submit">Login</button>
 </form>
