4bcdcd311455587f6d728a0cee5fd39eaed685ea
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-23 09:19:46 +0530

    Progressive Enhancement

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import { enhance } from '$app/forms';
 	export let form;
 
 	const redirectTo = $page.url.searchParams.get('redirectTo') || '/';
@@ -20,7 +21,7 @@
 	// };
 </script>
 
-<form method="post" action="?/login&redirectTo={redirectTo}">
+<form method="post" action="?/login&redirectTo={redirectTo}" use:enhance>
 	<p>{form?.message || ''}</p>
 	<input type="text" name="username" placeholder="username" value={form?.username ?? ''} />
 	<input type="password" name="password" placeholder="Password" />
